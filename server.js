bd0a8e2312022a76702803c1731a8880f205d2ac
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-10-11 18:59:45 -0600

    added git ignore

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
 const inquirer = require('inquirer');
-const express = require('express');
-const mysql = require('mysql2');
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Sql123!',
-//     database: '',
-//   },
-//   console.log(`Connected to the classlist_db database.`)
-//  );
-
-// db.query('', function (err, results) {
-//   console.log(results);
-// });
 
 const startPrompt = [
   {
